a4aad0b9a8ed87f9e935563b8936cce9f5fedce6
Carlos <carlosguzmantejeda13@gmail.com>
2023-05-04 18:12:12 -0400

    Created if else that checks whether user is over 18.

--- myProject.js
+++ myProject.js
@@ -1,5 +1,25 @@
 
+//import package that allows user input
 const prompt = require("prompt-sync")();
 
-console.log("Hi welcome to my gambling machine");
-const userAge = prompt("Enter your age and we will determine whether you can use the machine or not");
+console.log("Hi welcome to my slot machine");
+const userAge = prompt("Enter your age and we will determine whether you can use the machine or not. ");
+
+if(userAge >= 18){
+
+    console.log("That is awesome you can use the gambling machine");
+
+}
+else{
+    console.log("Sorry but you cannot use the slot machines because you are under 18.");
+}
+
+
+
+
+
+
+
+//function definitions
+
+
