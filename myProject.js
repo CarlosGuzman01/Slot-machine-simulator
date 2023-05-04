36e9819090796a50cde432bfabe77cc5e3c6a726
Carlos <carlosguzmantejeda13@gmail.com>
2023-05-04 19:37:36 -0400

    before making a main function

--- myProject.js
+++ myProject.js
@@ -2,24 +2,43 @@
 //import package that allows user input
 const prompt = require("prompt-sync")();
 
-console.log("Hi welcome to my slot machine");
-const userAge = prompt("Enter your age and we will determine whether you can use the machine or not. ");
 
-if(userAge >= 18){
 
-    console.log("That is awesome you can use the gambling machine");
+legal(userAge);
+
+
+
+
+
+
+
+function welcomeMessage()
+{
+    console.log("Hi welcome to my slot machine");
+    const userAge = prompt("Enter your age and we will determine whether you can use the slot machine or not. ");
+
+    return userAge;
+
 
 }
-else{
-    console.log("Sorry but you cannot use the slot machines because you are under 18.");
-}
-
-
-
-
-
-
 
 //function definitions
+function legal(userAge) {
+
+    if(userAge >= 18){
+
+        console.log("That is awesome you can use the gambling machine");
+        
+    
+    }
+    else{
+        console.log("Sorry but you cannot use the slot machines because you are under 18.");
+    }
+
+}
 
 
+
+
+
+const moneyAmount = prompt("Enter the amount of money you want to deposit: ");
