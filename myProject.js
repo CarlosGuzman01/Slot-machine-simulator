36a14d89545ffc3bd13be6a71c856fcecf0396ca
Carlos <carlosguzmantejeda13@gmail.com>
2023-05-04 17:52:30 -0400

    created the project

---
+++ myProject.js
@@ -0,0 +1,5 @@
+
+const prompt = require("prompt-sync")();
+
+console.log("Hi welcome to my gambling machine");
+const userAge = prompt("Enter your age and we will determine whether you can use the machine or not");
